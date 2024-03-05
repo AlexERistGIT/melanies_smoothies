70a8f2573e53ef42a0f8082dae9a83918b163c2a
AlexERistGIT <162336590+AlexERistGIT@users.noreply.github.com>
2024-03-05 18:03:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,10 +59,12 @@ if ingredients_list:
     # oder 
     # st.text(f'Your selection is:' + f'{incredients_list}') 
     # or for each_fruit in ingredients_list:
-    for x in ingredients_list:
-        ingredients_string += x + ' '
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
         ############################Ingrediente darstellen
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen+' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
         # st.text(fruityvice_response.json()) # json response zurückgeben als Text
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True) # response als dataframe darstellen
     # st.write('You selection is:', ingredients_string)
